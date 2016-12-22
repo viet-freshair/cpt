fb0b6a843eb8191f9813f979cc7c95cf9ab30640
Leo Nguyen <duyngha@gmail.com>
2016-12-22 02:48:35 +0000

    add tests files

---
+++ tests/ProviderTest.php
@@ -0,0 +1,13 @@
+<?php
+use PHPUnit\Framework\TestCase;
+
+class ProviderTest extends TestCase
+{
+    /**
+     * @test getArgs
+     */
+    public function testGetArgs()
+    {
+        
+    }
+}
